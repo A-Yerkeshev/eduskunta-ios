044642c50deca2f8022129bd279d4904056543c2
Arman Yerkesh <a.yerkesh@gmail.com>
2024-11-04 09:54:05 +0200

    Initial Commit

---
+++ Eduskunta-iOS/Eduskunta_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eduskunta_iOSApp.swift
+//  Eduskunta-iOS
+//
+//  Created by Arman Yerkeshev on 4.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Eduskunta_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Eduskunta-iOSTests/Eduskunta_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Eduskunta_iOSTests.swift
+//  Eduskunta-iOSTests
+//
+//  Created by Arman Yerkeshev on 4.11.2024.
+//
+
+import Testing
+@testable import Eduskunta_iOS
+
+struct Eduskunta_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
